4c3a7bffdffd01942948409dea43d625909f89fb
unknown <buithehuy.work@gmail.com>
2026-02-12 10:52:16 +0700

    fix install_updated

--- install_updated.sh
+++ install_updated.sh
@@ -4,7 +4,7 @@
 
 
 # install requirements
-pip install cython "numpy<2.0" ninja
+pip install "pip<24.1" "setuptools<71" cython "numpy<2.0" ninja
 
 
 pip install torch==1.9.1+cu111 torchaudio -f https://download.pytorch.org/whl/cu111/torch_stable.html
@@ -18,8 +18,10 @@ pip install ogb==1.3.2
 pip install rdkit-pypi==2021.9.3
 pip install dgl==0.7.2 -f https://data.dgl.ai/wheels/repo.html
 
+pip install omegaconf==2.0.6 hydra-core==1.0.7 sacrebleu regex bitarray
+
 cd fairseq
 # if fairseq submodule has not been checkouted, run:
 # git submodule update --init --recursive
-pip install .
+pip install --no-deps .
 python setup.py build_ext --inplace
